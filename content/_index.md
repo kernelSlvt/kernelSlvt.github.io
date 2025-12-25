9965416d0b7dac9b935a5a1ff23b3742a420c606
sarthak <sarthaktomar2143@gmail.com>
2025-12-26 03:35:20 +0530

    make separate page for blog index page to showcase all posts

---
+++ content/_index.md
@@ -0,0 +1,15 @@
+title: sλrthak 
+date: 2025-12-22
+-----
+
+hallo there!! im a second year cs undergrad. i am interested in **deep learning**, **systems programming** and **high performance computing**. i post about my work and interests here.
+
+im trying to discover more interesting things out there and spend time learning and reflecting on them. witnessing the emerging effects of a dozen matmuls is incredible to me. long term goal here is to understand in order to build intelligence.
+
+open to **new roles** in the above fields (and more!!)
+
+c++ // ocaml // rust // python // ts // golang // haskell
+
+> "waking up is the second hardest thing in the morning"
+>
+> -- me
--- src/main.py
+++ src/main.py
@@ -3,7 +3,11 @@ import shutil
 import time
 from pathlib import Path
 
-from utils import copy_files, generate_page, generate_pages_recursive
+from utils import (
+  copy_files,
+  generate_page,
+  generate_pages_recursive,
+)
 
 logger: logging.Logger = logging.getLogger(__name__)
 
@@ -22,15 +26,21 @@ def main() -> None:
   copy_files(src_path, dest_path)
 
   # generate the index page
-  index_path: Path = Path("./content/index.md")
+  index_path: Path = Path("./content/_index.md")
   index_template_path: Path = Path("./templates/home.html")
   index_dest_path: Path = Path("./public/index.html")
   generate_page(index_path, index_template_path, index_dest_path)
 
+  # generate the blog index page
+  blog_index_path: Path = Path("./content/blog/_index.md")
+  blog_index_template_path: Path = Path("./templates/blog_index.html")
+  blog_index_dest_path: Path = Path("./public/blog/index.html")
+  generate_page(blog_index_path, blog_index_template_path, blog_index_dest_path)
+
   # generate the blogs dir
-  blogs_path: Path = Path("./content/blog")
+  blogs_path: Path = Path("./content/")
   blog_template_path: Path = Path("./templates/blogs.html")
-  blogs_dest_path: Path = Path("./public/blog")
+  blogs_dest_path: Path = Path("./public/")
   generate_pages_recursive(blogs_path, blog_template_path, blogs_dest_path)
 
   logging.info(f"FINISHED at {time.asctime()}\n")
