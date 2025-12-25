2d4ebead945c9a6f9004ecdf7686bf037026adbd
sarthak <sarthaktomar2143@gmail.com>
2025-12-26 03:36:10 +0530

    add recent posts on homepage and use config file to get banner and contact

--- src/utils.py
+++ src/utils.py
@@ -21,7 +21,7 @@ def extract_metadata(markdown: str) -> list[str]:
     if line.startswith("date"):
       # print(line.split("date:", 1))
       date_ = line.split("date:", 1)[1].strip()
-      date_ = date.fromisoformat(date_).strftime("%d %b, %Y")
+      date_ = date.fromisoformat(date_)  # pyright: ignore[reportAssignmentType]
 
   if not title or not date_:
     raise Exception("Invalid metadata provided.")
@@ -59,6 +59,8 @@ def generate_pages_recursive(
     dest_path: Path = dest_dir_path / content
 
     if file_path.is_file():
+      if file_path.name.startswith("_"):
+        continue
       generate_page(file_path, template_path, dest_path)
       dest_path.rename(dest_path.with_suffix(".html"))
     else:
@@ -79,17 +81,105 @@ def generate_page(from_path: Path, template_path: Path, dest_path: Path) -> None
 
   metadata: list[str] = extract_metadata(markdown)
   title: str = metadata[0]
-  date_: str = metadata[1]
+  date_: str = metadata[1].strftime("%d %b, %Y")  # pyright: ignore[reportAttributeAccessIssue]
 
   configs: dict[str, str] = load_config()
   name: str = configs["name"]
+  banner_img: str = configs["banner"]
+  banner_link: str = f"<img src = '{banner_img}' alt = 'banner image'>"
+  contact_dict = configs["contact"]
+
+  blogs_path: Path = Path("./content/blog")
+  blog_data: dict[str, list[str]] = walk_blogs(blogs_path)
+  all_blogs_data: str = get_all_blogs(blog_data)
+  recent_blogs_data: str = get_recent_blogs(blog_data)
+  contact_list: str = get_contact_list(contact_dict)
 
   html: str = (
     templ.replace("{{ Name }}", name)
     .replace("{{ Title }}", title)
     .replace("{{ Date }}", date_)
     .replace("{{ Content }}", content)
+    .replace("{{ All_blogs }}", all_blogs_data)
+    .replace("{{ Recent_blogs }}", recent_blogs_data)
+    .replace("{{ Banner_image }}", banner_link)
+    .replace("{{ Contact }}", contact_list)
   )
 
   dest_path.parent.mkdir(parents=True, exist_ok=True)
   dest_path.write_text(html)
+
+
+def walk_blogs(blog_path: Path) -> dict[str, list[str]]:
+  if not blog_path.exists() or not blog_path.is_dir():
+    raise Exception("Invalid blog path passed!")
+
+  data: dict[str, list[str]] = {}
+  contents: list[str] = os.listdir(blog_path)
+  contents_path: list[Path] = [blog_path / content for content in contents]
+
+  for content in contents_path:
+    if content.is_dir():
+      file_path: Path = Path(content / "index.md")
+      metadata: list[str] = extract_metadata(file_path.read_text())
+      title: str = metadata[0]
+      date_: str = metadata[1]
+      data[str(content.name)] = [title, date_]
+
+  # sorting the dict based on the date
+  sorted_items: list[tuple[str, list[str]]] = sorted(
+    data.items(), key=lambda x: x[1][1], reverse=True
+  )
+  sorted_dict: dict[str, list[str]] = dict(sorted_items)
+
+  return sorted_dict
+
+
+def get_all_blogs(blog_data: dict[str, list[str]]) -> str:
+  blog_str: str = ""
+  for link in blog_data:
+    title: str = blog_data[link][0]
+    date: str = blog_data[link][1].strftime("%d %b, %Y")  # pyright: ignore[reportAttributeAccessIssue]
+    para_str: str = f'<p class = "blog-flex"><span><i>{date}</i></span><a href = "/blog/{link}">{title}</a></p>'
+    blog_str += para_str
+  return blog_str
+
+
+def get_recent_blogs(blog_data: dict[str, list[str]]) -> str:
+  blog_str: str = ""
+  count: int = 0
+  max_count: int = min(5, len(blog_data))
+  for link in blog_data:
+    count += 1
+    if count > max_count:
+      break
+    title: str = blog_data[link][0]
+    li_str: str = f'<li><a href = "/blog/{link}">{title}</a></li>'
+    blog_str += li_str
+  return blog_str
+
+
+def get_contact_list(contact_list) -> str:
+  contact_str: str = ""
+  for contact in contact_list:
+    value = contact_list[contact]
+    match contact:
+      case "github":
+        contact_str += (
+          f'<li><a href = "https://github.com/{value}">{contact} @{value}</a></li>'
+        )
+      case "mail":
+        contact_str += f'<li><a href = "mailto:{value}">{contact} {value}</a></li>'
+      case "twitter/x":
+        contact_str += (
+          f'<li><a href = "https://x.com/{value}">{contact} @{value}</a></li>'
+        )
+      case "discord":
+        contact_str += f'<li><a href = "https://discord.com/users/{value[1]}">{contact} @{value[0]}</a></li>'
+      case "linkedin":
+        contact_str += f'<li><a href = "https://linked.com/in/{value}">{contact} /in/{value}</a></li>'
+      case "instagram":
+        contact_str += (
+          f'<li><a href = "https://instagram.com/{value}">{contact} @{value}</a></li>'
+        )
+  return contact_str
