786dc736619723e2541660d4d0df3ec2bf54b18b
sarthak <shinero_voldemort@protonmail.com>
2026-01-12 14:47:10 +0530

    add docs/

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ def main() -> None:
   logging.info(f"STARTED at {time.asctime()}")
 
   # clean the public/
-  dest_path: Path = Path("./public")
+  dest_path: Path = Path("./docs")
   src_path: Path = Path("./static")
   if dest_path.exists():
     shutil.rmtree(dest_path)
@@ -28,19 +28,19 @@ def main() -> None:
   # generate the index page
   index_path: Path = Path("./content/_index.md")
   index_template_path: Path = Path("./templates/home.html")
-  index_dest_path: Path = Path("./public/index.html")
+  index_dest_path: Path = Path("./docs/index.html")
   generate_page(index_path, index_template_path, index_dest_path)
 
   # generate the blog index page
   blog_index_path: Path = Path("./content/blog/_index.md")
   blog_index_template_path: Path = Path("./templates/blog_index.html")
-  blog_index_dest_path: Path = Path("./public/blog/index.html")
+  blog_index_dest_path: Path = Path("./docs/blog/index.html")
   generate_page(blog_index_path, blog_index_template_path, blog_index_dest_path)
 
   # generate the blogs dir
   blogs_path: Path = Path("./content/")
   blog_template_path: Path = Path("./templates/blogs.html")
-  blogs_dest_path: Path = Path("./public/")
+  blogs_dest_path: Path = Path("./docs/")
   generate_pages_recursive(blogs_path, blog_template_path, blogs_dest_path)
 
   logging.info(f"FINISHED at {time.asctime()}\n")
